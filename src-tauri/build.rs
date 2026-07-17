08acc5b758c2477cebbe61ffa9ea971b75d92808
sonu sharma <echosonusharma@gmail.com>
2026-07-17 10:17:22 +0530

    feat: andriod apk build setup

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("android") {
+        println!("cargo:rustc-link-arg=-Wl,-z,max-page-size=16384");
+        println!("cargo:rustc-link-arg=-Wl,-z,common-page-size=16384");
+    }
     tauri_build::build()
 }
