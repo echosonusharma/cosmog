5a4771a0a3d2e39895d270cad521d972590fae29
sonu sharma <echosonusharma@gmail.com>
2026-07-17 10:17:22 +0530

    feat: andriod apk build setup

---
+++ src-tauri/gen/android/app/src/main/java/com/sonus/cosmog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sonus.cosmog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
